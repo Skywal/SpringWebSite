441a85f700ec9f713de1fd6d861cdc5ab1d164fa
Bloom <skaywoker6@gmail.com>
2018-08-16 13:05:16 +0300

    Add WebConfig class with some configs

---
+++ src/main/java/com/bloom/springwebpage/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.bloom.springwebpage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver getViewResolver(){
+        FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+        freeMarkerViewResolver.setOrder(1); // порядок у якому буде запускатися
+        freeMarkerViewResolver.setSuffix(".ftl"); // розібратися
+        freeMarkerViewResolver.setPrefix("");
+        return freeMarkerViewResolver;
+    }
+    @Bean
+    public FreeMarkerConfigurer getFreeMarkerConfigurer(){
+        // де лежатимуть template
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPaths("/", "/WEB-INF/views"); //задаємо декілька шляхів до template
+        return freeMarkerConfigurer;
+    }
+}
